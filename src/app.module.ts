fcc0a538fd22a87728b278d9032f7b9516194f8b
RyoLee <lyuanzhe9@gmail.com>
2026-03-09 23:19:37 +0800

    feat(core):初始化各个模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
+import { OssModule } from './oss/oss.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, ConfigModule, OssModule, AuthModule, UserModule, BookModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
